98358d864b8ba691c09363820284d9a6278fa6ef
juno@millie.town <hackish113@gmail.com>
2025-05-29 10:55:31 +0900

    feat(types): PointOption 및 PointsGroup 타입 추가, WindowSize 타입 정의

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,1 +1,14 @@
 export type Point = [number, number, number]
+export type PointOption = {
+  color: string
+  range: Range
+}
+export type PointsGroup = {
+  points: Array<Point>
+  option: PointOption
+}
+export type WindowSize = {
+  width: number
+  height: number
+  ratio: number
+}

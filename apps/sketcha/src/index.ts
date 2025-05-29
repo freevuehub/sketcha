895bd49c2a606cebb9afcc8d472443817e76f8f7
juno@millie.town <hackish113@gmail.com>
2025-05-29 10:54:45 +0900

    feat(sketcha): 스케치 애플리케이션에 캔버스 타입 및 색상 설정 기능 추가

--- apps/sketcha/src/index.ts
+++ apps/sketcha/src/index.ts
@@ -1,5 +1,5 @@
-import { isUndefined, pipe, throwIf } from '@fxts/core'
-import { ROOT_CSS_TEXT } from '@sketcha/constant'
+import { isNull, isUndefined, pipe, throwIf } from '@fxts/core'
+import { ROOT_CSS_TEXT, CanvasType, ErrorMessage } from '@sketcha/constant'
 
 import './components'
 
@@ -8,25 +8,61 @@ type Props = {
 }
 
 class Sketcha {
+  private shadowRoot: ShadowRoot | null = null
+
+  color: string = '#aaaaaa'
+
   constructor(props: Props) {
     pipe(
       props.element,
       throwIf(isUndefined, () => Error('')),
       (element) => {
-        element.innerHTML = ''
+        element.style.position = 'relative !important'
 
-        return element
+        const createWrapper = document.createElement('div')
+
+        createWrapper.style.position = 'relative !important'
+        createWrapper.id = 'sketcha-wrapper'
+
+        element.append(createWrapper)
+
+        return createWrapper
       },
-      (element) => element.attachShadow({ mode: 'open' }),
+      (element) => element.attachShadow({ mode: 'closed' }),
       (shadowRoot) => {
         const styleSheet = new CSSStyleSheet()
 
         styleSheet.replaceSync(ROOT_CSS_TEXT)
 
         shadowRoot.adoptedStyleSheets = [styleSheet]
-        shadowRoot.innerHTML = `<sketcha-canvas></sketcha-canvas>`
+        shadowRoot.innerHTML = `
+          <sketcha-canvas>
+            <canvas is="sketcha-async-canvas" type="${CanvasType.ASYNC}"></canvas>
+            <canvas is="sketcha-sync-canvas" color="${this.color}" type="${CanvasType.SYNC}"></canvas>
+          </sketcha-canvas>
+        `
+
+        this.shadowRoot = shadowRoot
       }
     )
+  }
+
+  set setColor(color: string) {
+    try {
+      pipe(
+        this.shadowRoot,
+        throwIf(isNull, () => Error(ErrorMessage.SKETCHA_NOT_INITIALIZED)),
+        (root) => root.querySelector(`canvas[type="${CanvasType.SYNC}"]`),
+        throwIf(isNull, () => Error(ErrorMessage.SKETCHA_SYNC_CANVAS_NOT_FOUND)),
+        (element) => {
+          element.setAttribute('color', color)
+        }
+      )
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.color = color
+    }
   }
 }
 

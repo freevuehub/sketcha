4cd9697124fcd6c69722869a52d21ed79c4538b8
juno@millie.town <hackish113@gmail.com>
2025-05-29 10:56:08 +0900

    refactor(Canvas): 포인터 드롭 이벤트 처리 개선 및 pointsGroup 관리 추가

--- apps/sketcha/src/components/Canvas/Async.ts
+++ apps/sketcha/src/components/Canvas/Async.ts
@@ -1,28 +1,42 @@
 import { CanvasType, EventListenerType } from '@sketcha/constant'
 import HTMLCanvasElement from './Common'
 import { getStroke, events, getSvgPath } from '@sketcha/utils'
-import type { Point } from '@sketcha/types'
+import type { Point, PointsGroup } from '@sketcha/types'
 
 class AsyncCanvasElement extends HTMLCanvasElement {
-  points: Point[] = []
+  pointsGroup: Map<string, PointsGroup> = new Map()
 
   constructor() {
     super()
 
-    this.render(CanvasType.ASYNC)
+    this.style.pointerEvents = 'none'
 
-    events.on(EventListenerType.POINT_DROP, (points) => {
-      this.points = points
+    events.on(EventListenerType.POINTER_DROP, (points, option) => {
+      this.setPoints = { points, option }
 
-      this.Context?.save()
-      this.Context?.scale(window.devicePixelRatio, window.devicePixelRatio)
+      this.Context.save()
+      this.Context.scale(window.devicePixelRatio, window.devicePixelRatio)
 
-      const path = new Path2D(getSvgPath(getStroke(this.points)))
+      const path = new Path2D(getSvgPath(getStroke(points)))
 
-      this.Context?.fill(path)
-      this.Context?.restore()
+      this.Context.fillStyle = option.color
+      this.Context.fill(path)
+      this.Context.restore()
     })
+
+    window.addEventListener('resize', () => {
+      this.Context.save()
+      this.Context.scale(window.devicePixelRatio, window.devicePixelRatio)
+    })
+  }
+
+  get getUUID() {
+    return Math.random().toString(36).substring(2, 15)
+  }
+
+  set setPoints(value: PointsGroup) {
+    this.pointsGroup.set(this.getUUID, value)
   }
 }
 
-customElements.define('sketcha-async-canvas', AsyncCanvasElement)
+customElements.define('sketcha-async-canvas', AsyncCanvasElement, { extends: 'canvas' })
